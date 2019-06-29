a6cc9636943268e41d0a0c2daa707ac57b2a7e32
Ballantyne, Bob <bob.ballantyne@emc.com>
2019-06-28 21:30:39 -0400

    fix config issues

--- build/usr/local/bin/run.sh
+++ build/usr/local/bin/run.sh
@@ -2,8 +2,14 @@
 cd "$KAFKA_USER_DIR"
 chmod -R a+rx "$KAFKA_USER_DIR/bin"
 
-if [ $# -eq 0 ] || [ $1 = 'broker' ]; then
-    bin/kafka-server-start.sh config/server.properties
-elif [ $1 = 'zookeeper' ]; then
-    bin/zookeeper-server-start.sh config/zookeeper.properties
-fi
+case "$1" in
+    broker)
+        "${KAFKA_USER_DIR}/bin/kafka-server-start" "${KAFKA_USER_DIR}/etc/kafka/server.properties"
+        ;;
+    zookeeper)
+        "${KAFKA_USER_DIR}/bin/zookeeper-server-start" "${KAFKA_USER_DIR}/etc/kafka/zookeeper.properties"
+        ;;
+    *)
+        term.log 'Invalid option passed to startup\n'
+        ;;
+esac
