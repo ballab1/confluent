542f018868dcbf489021c679acf5c35201fc1eef
Ballantyne, Bob <bob.ballantyne@emc.com>
2020-02-09 10:38:22 -0500

    add support for v5.4.0

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # confluent
+
+Community sources/sinks
+    https://cwiki.apache.org/confluence/display/KAFKA/Ecosystem
+
+BROKER
+    https://kafka.apache.org/documentation/#connectconfigs
+    config in /usr/local/confluent/etc/kafka/server.properties
+
+ZOOKEEPER:
+    docs:   https://zookeeper.apache.org/doc/r3.4.8/zookeeperAdmin.html
+    config in /usr/local/confluent/etc/kafka/zookeeper.properties
+
+    Commands: The Four Letter Words enabled
--- build/action_folders/07.run.startup/04.confluent.sh
+++ build/action_folders/07.run.startup/04.confluent.sh
@@ -1,29 +1,78 @@
 #!/bin/bash
 
+#----------------------------------------------------------------------------------------------
+function kaf_match()
+{
+    echo "${1,,}"
+}
+
+function zoo_match()
+{
+    local in="${1,,}"
+
+    local keyWord
+    for keyWord in "${zookeeper_keywords[@]}"; do
+        if [ "${keyWord,,}" = "$in" ] || [ "${keyWord,,}" = "${in//./}" ]; then
+            echo "$keyWord"
+            return 0
+        fi
+    done
+    echo "$in"
+    return 0
+}
+#----------------------------------------------------------------------------------------------
+function my.updateConfigFromENV()
+{
+    local -r env_prefix="${1:?"Input parameter 'env_prefix' must be passed to 'function ${FUNCNAME[0]}()'"}"
+    local -r config_file="${2:?"Input parameter 'config_file' must be passed to 'function ${FUNCNAME[0]}()'"}"
+    local -r fn="${3:?"Input parameter 'fn' must be passed to 'function ${FUNCNAME[0]}()'"}"
+
+    [ -f "$config_file" ] || trap.die 'Property file does not exist'
+
+    local key
+    for key in $(env | grep -E "^$env_prefix"); do
+        key="${key#$env_prefix}"
+        local val=${key##*=}
+        key="${key%%=*}"
+        key=$($fn "${key//_/.}")
+        lib.updatePropertyFile "$key" "$val" "$config_file" 'must_exist'
+    done
+}
+#----------------------------------------------------------------------------------------------
+
+
+declare -a zookeeper_keywords=( 'autopurge.purgeInterval'     'autopurge.snapRetainCount'   'clientPort'
+                                'clientPortAddress'           'cnxTimeout'                  'dataDir'
+                                'dataLogDir'                  'electionAlg'                 'forceSync'
+                                'globalOutstandingLimit'      'initLimit'                   'leaderServes'
+                                'maxClientCnxns'              'maxSessionTimeout'           'minSessionTimeout'
+                                'preAllocSize'                'quorumListenOnAllIPs'        'skipACL'
+                                'snapCount'                   'syncEnabled'                 'syncLimit'
+                                'tickTime'                    'traceFile' )
+
+
 : ${KAFKA_UID:?"Environment variable 'KAFKA_UID' not defined in '${BASH_SOURCE[0]}'"}
+[ "${KAFKA_BROKER_ID:-0}" -eq 0 ] && [ "${ZOOKEEPER_SERVER_ID:-0}" -eq 0 ] && trap.die 'invalid configuration: need either KAFKA_BROKER_ID or ZOOKEEPER_SERVER_ID defined'
+[ "${KAFKA_BROKER_ID:-0}" -gt 0 ] && [ "${ZOOKEEPER_SERVER_ID:-0}" -gt 0 ] && trap.die "invalid configuration: cannot have both KAFKA_BROKER_ID: $KAFKA_BROKER_ID and ZOOKEEPER_SERVER_ID: $ZOOKEEPER_SERVER_ID defined"
 
-crf.fixupDirectory "$KAFKA_USER_DIR" "$KAFKA_UID"
-crf.fixupDirectory "$KAFKA_LOG_DIRS" "$KAFKA_UID"
-crf.fixupDirectory "$KAFKA_CONF_DIR" "$KAFKA_UID"
-
-crf.fixupDirectory "$ZOOKEEPER_DATA_DIR" "$KAFKA_UID"
-crf.fixupDirectory "$ZOOKEEPER_LOG_DIR" "$KAFKA_UID"
-crf.fixupDirectory "$ZOOKEEPER_CONF_DIR" "$KAFKA_UID"
 
 chmod -R a+rx "$KAFKA_USER_DIR/bin"
 
+if [ "${KAFKA_BROKER_ID:-0}" -gt 0 ]; then
+    crf.fixupDirectory "$KAFKA_USER_DIR" "$KAFKA_UID"
+    crf.fixupDirectory "$KAFKA_LOG_DIRS" "$KAFKA_UID"
+    crf.fixupDirectory "$KAFKA_CONF_DIR" "$KAFKA_UID"
 
-if [ ${KAFKA_BROKER_ID:-} -gt 0 ]; then
     # Generate the config only if it doesn't exist
     declare config="$KAFKA_CONF_DIR/server.properties"
     if [ -f "$config" ]; then
-        lib.updateConfigFromENV 'KAFKA_' "$config"
+        my.updateConfigFromENV 'KAFKA_' "$config" 'kaf_match'
         [ "${ZOOKEEPER_TIMEOUT:-}" ] && sed -i "s|^zookeeper.connection.timeout.ms=.*$|zookeeper.connection.timeout.ms=$ZOOKEEPER_TIMEOUT|" "$config"
     fi
 
     printf '\n\nCurrent config: %s\n' "$config"
-    cd "$KAFKA_USER_DIR"
-    while read ln; do
+    cd "$KAFKA_USER_DIR" ||:
+    while read -r ln; do
         printf '    %s\n' "$ln"
     done < <(cat "$config")
 
@@ -32,23 +81,22 @@ if [ ${KAFKA_BROKER_ID:-} -gt 0 ]; then
 fi
 
 
-if [ ${ZOOKEEPER_MY_ID:-} -gt 0 ]; then
+if [ "${ZOOKEEPER_SERVER_ID:-0}" -gt 0 ]; then
+    crf.fixupDirectory "$ZOOKEEPER_DATADIR" "$KAFKA_UID"
+    crf.fixupDirectory "$ZOOKEEPER_DATALOGDIR" "$KAFKA_UID"
+    crf.fixupDirectory "$ZOOKEEPER_CONFDIR" "$KAFKA_UID"
+
     # Write myid only if it doesn't exist
-    [ "${ZOOKEEPER_MY_ID:-}" ]  && echo "$ZOOKEEPER_MY_ID" > "$ZOOKEEPER_DATA_DIR/myid"
+    echo "$ZOOKEEPER_SERVER_ID" > "$ZOOKEEPER_DATADIR/myid"
+    unset ZOOKEEPER_SERVER_ID
 
     # Generate the config only if it doesn't exist
-    config="${KAFKA_CONF_DIR}/zookeeper.properties"
-    if [ -f "$config" ]; then
-        lib.updateConfigFromENV 'ZOOKEEPER_' "$config"
-        [ "${ZOOKEEPER_PORT:-}" ]      && sed -i "s|^clientPort=.*$|clientPort=$ZOOKEEPER_PORT|" "$config"
-        [ "${ZOOKEEPER_DATA_DIR:-}" ]  && sed -i "s|^dataDir=.*$|dataDir=$ZOOKEEPER_DATA_DIR|" "$config"
-        [ "${ZOOKEEPER_LOG_DIR:-}" ]   && sed -i "s|^dataLogDir=.*$|dataLogDir=$ZOOKEEPER_LOG_DIR|" "$config"
-        [ "${ZOOKEEPER_TICK_TIME:-}" ] && sed -i "s|^tickTime=.*$|tickTime=$ZOOKEEPER_TICK_TIME|" "$config"
-    fi
+    config="${ZOOKEEPER_CONFDIR}/zookeeper.properties"
+    [ -f "$config" ] && my.updateConfigFromENV 'ZOOKEEPER_' "$config" 'zoo_match'
 
     printf '\n\nCurrent config: %s\n' "$config"
-    cd "$KAFKA_USER_DIR"
-    while read ln; do
+    cd "$KAFKA_USER_DIR" ||:
+    while read -r ln; do
         printf '    %s\n' "$ln"
     done < <(cat "$config")
 
