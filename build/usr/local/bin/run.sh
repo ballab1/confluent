37f093b8dcbb9170a46928ff7c49efcd25010810
Ballantyne, Bob <ballantyne.robert@gmail.com>
2025-07-18 20:20:07 -0400

    update proior to redpanda adoption

--- build/usr/local/bin/run.sh
+++ build/usr/local/bin/run.sh
@@ -5,12 +5,12 @@ chmod -R a+rx "$KAFKA_USER_DIR/bin"
 
 case "$1" in
     broker)
-        "${KAFKA_USER_DIR}/bin/kafka-server-start" "${KAFKA_USER_DIR}/etc/kafka/server.properties"
         ;;
     zookeeper)
-        "${KAFKA_USER_DIR}/bin/zookeeper-server-start" "${KAFKA_USER_DIR}/etc/kafka/zookeeper.properties"
         ;;
     *)
         term.log 'Invalid option passed to startup\n'
+        exit 1
         ;;
 esac
+"${KAFKA_USER_DIR}/bin/kafka-server-start" "${KAFKA_USER_DIR}/etc/kafka/${1}.properties"
